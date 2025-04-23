52a2a8728dbeebb333b594e87278920b6e0d600b
chado <a.sale@outlook.fr>
2025-04-23 09:44:12 +0200

    fix download link

--- pages/about.vue
+++ pages/about.vue
@@ -77,11 +77,11 @@ useHead({
       </div>
 
       <div class="text-center">
-        <NuxtLink href="/CV_FULL_SATCK_Alexandre_Salé.pdf" target="_blank"
-                  class="inline-flex items-center px-6 py-3 mt-4 text-white bg-primary rounded-lg hover:bg-secondary transition">
+        <a href="/CV_FULL_SATCK_Alexandre_Salé.pdf" download
+           class="inline-flex items-center px-6 py-3 mt-4 text-white bg-primary rounded-lg hover:bg-secondary transition">
           <Icon name="lucide:download" class="w-5 h-5 mr-2" />
           Télécharger mon CV
-        </NuxtLink>
+        </a>
       </div>
     </div>
   </section>
