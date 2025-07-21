4ba3e94792d98bc0a0f9c8723c8d835e095e0850
chado <a.sale@outlook.fr>
2025-07-21 10:04:22 +0200

    Refactor structure about project to split frontend and backend

--- i18n.config.ts
+++
@@ -1,6 +0,0 @@
-import {defineI18nConfig} from "@nuxtjs/i18n/dist/runtime/composables";
-
-export default defineI18nConfig(() => ({
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  legacy: false,
-}))
