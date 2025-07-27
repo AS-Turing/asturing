8bfc6d364a73e786d3e9d40b8c8053dbe6acb932
chado <a.sale@outlook.fr>
2025-07-27 09:44:10 +0200

    Refactor localisation components to use Localisation interface, update dynamic routing, and improve localisation page content and structure.

---
+++ frontend/types/localisation.ts
@@ -0,0 +1,11 @@
+export interface Localisation {
+    id: number
+    city: string
+    slug: string
+    title: string
+    description: string
+    ogTitle: string
+    ogDescription: string
+    ogUrl: string
+    map: string
+}
