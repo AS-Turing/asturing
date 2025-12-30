45f601df1cd91b335722881583287c5219ff873b
chado <a.sale@outlook.fr>
2025-12-30 22:13:15 +0100

    Update GTM in nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -40,17 +40,13 @@ export default defineNuxtConfig({
       },
       script: [
         {
-          children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N49FF724');`,
+          children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-TK6GRG67');`,
           type: 'text/javascript'
         }
       ],
       noscript: [
         {
-          children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N49FF724" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK6GRG67" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
         }
       ],
       link: [
