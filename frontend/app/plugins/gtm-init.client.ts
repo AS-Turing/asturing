22a6b3fdd99fe835d97f7593f7ec66b7a85985ba
chado <a.sale@outlook.fr>
2026-01-04 22:48:35 +0100

    fix(gtm): utilise useScript() au lieu de useHead pour GTM

--- frontend/app/plugins/gtm-init.client.ts
+++ frontend/app/plugins/gtm-init.client.ts
@@ -1,33 +1,20 @@
 /**
  * Plugin GTM - Charge Google Tag Manager
- * Doit être chargé côté client uniquement
+ * Utilise l'approche recommandée pour Nuxt 4
  */
 export default defineNuxtPlugin(() => {
-  // Utilise useHead pour injecter GTM dans le head
-  useHead({
-    script: [
-      {
-        innerHTML: `
-          (function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),
-                dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TK6GRG67');
-        `,
-        type: 'text/javascript',
-        tagPosition: 'head'
-      }
-    ],
-    noscript: [
-      {
-        innerHTML: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK6GRG67" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
-        tagPosition: 'bodyOpen'
-      }
-    ]
+  // Initialise immédiatement le dataLayer
+  if (typeof window !== 'undefined') {
+    window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    })
+  }
+
+  // Charge le script GTM avec useScript
+  useScript({
+    src: 'https://www.googletagmanager.com/gtm.js?id=GTM-TK6GRG67',
+    async: true
   })
 })
