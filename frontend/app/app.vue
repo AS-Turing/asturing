4a1b47138fad7bfa8a01ea8405adb5ece7ecfa96
chado <a.sale@outlook.fr>
2026-01-04 22:54:19 +0100

    fix(gtm): déplace GTM dans app.vue en client-only
    - Supprime le plugin gtm-init qui ne fonctionnait pas
    - Injecte GTM directement dans app.vue avec ClientOnly
    - Initialise dataLayer immédiatement côté client
    - Fix définitif du problème de chargement GTM

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,26 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-    <MainNotifications />
-  </NuxtLayout>
+  <div>
+    <!-- GTM Script -->
+    <ClientOnly>
+      <component :is="'script'">
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TK6GRG67');
+      </component>
+    </ClientOnly>
+    
+    <NuxtLayout>
+      <NuxtPage />
+      <MainNotifications />
+    </NuxtLayout>
+  </div>
 </template>
+
+<script setup>
+// Initialise dataLayer dès que possible côté client
+if (import.meta.client) {
+  window.dataLayer = window.dataLayer || []
+}
+</script>
