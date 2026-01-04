50936e3641a827c102c8b1461c908e350242ad46
chado <a.sale@outlook.fr>
2026-01-04 22:43:15 +0100

    fix(gtm): utilise useHead dans le plugin pour injection GTM

--- frontend/app/plugins/gtm-init.client.ts
+++ frontend/app/plugins/gtm-init.client.ts
@@ -3,33 +3,31 @@
  * Doit être chargé côté client uniquement
  */
 export default defineNuxtPlugin(() => {
-  // Initialise dataLayer
-  window.dataLayer = window.dataLayer || []
-  
-  // Pousse l'événement de démarrage GTM
-  window.dataLayer.push({
-    'gtm.start': new Date().getTime(),
-    event: 'gtm.js'
+  // Utilise useHead pour injecter GTM dans le head
+  useHead({
+    script: [
+      {
+        innerHTML: `
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TK6GRG67');
+        `,
+        type: 'text/javascript',
+        tagPosition: 'head'
+      }
+    ],
+    noscript: [
+      {
+        innerHTML: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK6GRG67" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+        tagPosition: 'bodyOpen'
+      }
+    ]
   })
-  
-  // Charge le script GTM
-  const script = document.createElement('script')
-  script.async = true
-  script.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-TK6GRG67'
-  
-  const firstScript = document.getElementsByTagName('script')[0]
-  firstScript.parentNode?.insertBefore(script, firstScript)
-  
-  // Ajoute le noscript iframe pour GTM
-  if (document.body) {
-    const noscript = document.createElement('noscript')
-    const iframe = document.createElement('iframe')
-    iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-TK6GRG67'
-    iframe.height = '0'
-    iframe.width = '0'
-    iframe.style.display = 'none'
-    iframe.style.visibility = 'hidden'
-    noscript.appendChild(iframe)
-    document.body.insertBefore(noscript, document.body.firstChild)
-  }
 })
